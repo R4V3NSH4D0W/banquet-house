0074c07070d2a0b159010c145cac5406bc0d49ae
R4V3NSH4D0W <88871970+R4V3NSH4D0W@users.noreply.github.com>
2023-01-26 18:01:43 +0545

    event

---
+++ adminpanel/content.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--=== Coding by CodingLab | www.codinglabweb.com === -->
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="style.css">
+
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <title>Content</title>
+</head>
+
+<body>
+    <?php
+include 'navbar.php';
+?>
+    <section class="dashboard">
+        <?php
+include 'search.php';
+?>
+    </section>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
