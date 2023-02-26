0e848fbce8e1b1e82acd7c3689f0a396a0c33714
Manish Shrestha <manishshrestha0803@gmail.com>
2023-02-26 17:43:48 +0545

    50% done review section

--- frontend/landingpage/js/script.js
+++ frontend/landingpage/js/script.js
@@ -18,3 +18,23 @@ var swiper = new Swiper(".home-slider", {
 });
 
 
+// var swiper = new Swiper(".review-slider", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 0,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 2,
+//     slideShadows: true,
+//   },
+//   loop: true,
+//   autoplay: {
+//     delay: 1000,
+//     disableOnInteraction: false,
+//   },
+// });
+
+
